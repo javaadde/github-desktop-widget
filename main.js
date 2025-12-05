06b12433d3da7bdab0d282e8bd652f2c8122660e
javad-kuttappi <javaadde123@hotmail.com>
2025-12-05 16:25:26 +0530

    added auto stariting when the console is on

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
+const AutoLaunch = require("auto-launch");
+
+// Configure auto-launch
+const autoLauncher = new AutoLaunch({
+  name: "GitHub Widget",
+  path: app.getPath("exe"),
+});
+
+// Enable auto-launch on first run
+autoLauncher.isEnabled().then((isEnabled) => {
+  if (!isEnabled) {
+    autoLauncher.enable();
+    console.log("Auto-launch enabled");
+  }
+}).catch((err) => {
+  console.error("Error checking auto-launch status:", err);
+});
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -21,6 +38,22 @@ function createWindow() {
   });
 
   win.loadFile("index.html");
+
+  // Optional: Add IPC handler to toggle auto-launch from renderer
+  ipcMain.handle("toggle-auto-launch", async () => {
+    const isEnabled = await autoLauncher.isEnabled();
+    if (isEnabled) {
+      await autoLauncher.disable();
+      return false;
+    } else {
+      await autoLauncher.enable();
+      return true;
+    }
+  });
+
+  ipcMain.handle("get-auto-launch-status", async () => {
+    return await autoLauncher.isEnabled();
+  });
 }
 
 app.whenReady().then(createWindow);
